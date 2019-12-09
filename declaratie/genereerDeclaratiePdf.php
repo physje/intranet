7bffdcbf86a0a30a131b0f733f7ec72ac37aac6d
tim2meijer <tim2meijer@hotmail.com>
2019-12-09 23:14:15 +0100

    Genereren van een declaratie pdf
    Initiele commit genereren van een declaratie pdf
    
    - Genereer_declaratie_pdf() ->succesvol getest
    - De header van de pdf moet nog even naar smaak aangepast worden. Ook moeten we even kijken wat we doen met het telefoonnummer van de penningmeester in de footer. Plain text toevoegen / helemaal weg laten / iets anders slims.

---
+++ declaratie/genereerDeclaratiePdf.php
@@ -0,0 +1,151 @@
+<?php
+include_once('../include/pdf/config.php');
+include_once('../include/pdf/3gk_declaratie_table.php');
+
+define('EURO',chr(128));
+
+/**
+ * Berken de betaal datum op basis van de declaratie datum
+ * @param  string $datum             datum al vergkregen door de date() functie
+ * @param  int    $betaalDag         dag van uitbetalen
+ * @return string $betaalDatum       betaal datum string formaat "d-m-Y"
+ */
+function bereken_betaal_datum($datum, $betaalDag) 
+{
+    $timestamp = strtotime($datum);
+    $betaalDatum = "";
+
+    $dag = date('d', $timestamp);
+    $maand = date('m', $timestamp);
+    $jaar = date('Y', $timestamp);
+
+    if ( $dag < $betaalDag ) {
+        $betaalDatum = $betaalDag.date('-m-Y', $timestamp);
+    } else {
+        if ( $maand == 12 ) {
+            $maand = "1";
+            $jaar += 1;
+        } else {
+            $maand += 1;
+        }
+        $betaalDatum = $betaalDag.$maand."-".$jaar;
+    }
+
+    return $betaalDatum;
+}
+
+/**
+ * Genereer pdf met declaratie gegevens ter administratie
+ * @param  string $mutatieNr        mutatie nummer
+ * @param  string $mutatieDatum     datum al vergkregen door de date() functie
+ * @param  string $naam             naam van de declarant
+ * @param  string $adres            adres van de declarant (indien niet bekend leeg laten)
+ * @param  string $mailadres        mailadres van de declarant (indien niet bekend leeg laten)
+ * @param  string $iban             naam van de declarant
+ * @param  array  $declaratieData   array met declaratieData: [['onderdeel1', 'bedrag'], [.., ...] ... ]
+ */
+function genereer_declaratie_pdf($mutatieNr, $mutatieDatum, $naam, $adres, $mailadres, $iban, $declaratieData)
+{
+    global $cfgMarge;
+    
+    $pdf = new PDF_3GK_Table_Declaratie;
+    $fontSize = 10;
+
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont("Helvetica",'',$fontSize);
+    $breedte = $pdf->GetPageWidth();
+
+    # Maak tabel met basis informatie over de declaratie
+    $betaalDatum = bereken_betaal_datum($mutatieDatum, 20);
+
+    $header = ["Declaratie Nr.", "Declaratie datum", "Verwachte betaaldatum"];
+    $data = [[$mutatieNr, $mutatieDatum, $betaalDatum]];
+    $widths = array_fill(1, (count($header)-1), ($breedte-30-(2*$cfgMarge))/(count($header)-1));
+    $widths[0] = 30;
+    $pdf->SetWidths($widths);
+    $pdf->makeTable($header, $data);
+
+    # General information
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+
+    $pdf->SetFont("Helvetica",'B',16);
+    $pdf->Write(5, "Contactgegevens");
+    $pdf->Ln();
+    $pdf->Ln();
+
+    $pdf->SetFont("Helvetica",'',$fontSize);
+    $pdf->Cell(25,5, "Naam:", 'R', 0, 'L', 0);
+    $pdf->Write(5, $naam);
+    $pdf->Ln();
+
+    if ( $adres != "" ) {
+        $pdf->Cell(25,5, "Adres:", 'R', 0, 'L', 0);
+        $pdf->Write(5, $adres);
+        $pdf->Ln();
+    }
+    
+    if ( $mailadres != "" ) {
+        $pdf->Cell(25,5, "Mailadres:", 'R', 0, 'L', 0);
+        $pdf->Write(5, $mailadres);
+        $pdf->Ln();
+    }
+    
+    $pdf->Cell(25,5, "IBAN:", 'R', 0, 'L', 0);
+    $pdf->Write(5, $iban);
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Ln();
+
+    $pdf->SetFont("Helvetica",'B',16);
+    $pdf->Write(5, "Declaratie overzicht");
+    $pdf->Ln();
+    $pdf->Ln();
+
+    $pdf->SetFont("Helvetica",'',$fontSize);
+
+    # Maak tabel met details over de declaratie 
+    $totaalBedrag = 0.0;
+    foreach ($declaratieData as $data) {
+        $totaalBedrag += (float) filter_var( $data[1], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+    }
+    $totaalBedragRow = ["Totaal uit te betalen bedrag:", "€ ".$totaalBedrag];
+
+    $header = ["Onderdeel", "Bedrag"];
+    $data = $declaratieData;
+    array_push($data, $totaalBedragRow);
+
+    $widths = array_fill(0, (count($header)), ($breedte-(2*$cfgMarge))/(count($header)));
+    $pdf->SetWidths($widths);
+    $pdf->makeTable($header, $data);
+
+    # Sub text
+    $pdf->SetFont("Helvetica",'I',$fontSize);
+    $pdf->SetY(-60);
+    $pdf->Write(5, "De declaratie is met succes ontvangen en we proberen het te declareren bedrag over te maken op of voor de bovengenoemde datum. Mochten er gegevens op deze declaratie niet kloppen neem dan contact op met de penningmeester van de Koningskerk via onderstaande contactgegevens. ");
+
+    # Genereer de pdf
+    $pdf->Output('I', $mutatieNr.'pdf');
+
+}
+
+/*
+$mutatieNr = "123_13719";
+$mutatieDatum = date("Y-m-d");
+$naam = "Jan Janssen";
+$adres = "adres 123"; 
+$mailadres = "naam@domein.nl";
+$iban = "NL01XXXX0123456789";
+$declaratieData = [["OnderdeelX", "€ 90.00"], ["OnderdeelY", "€ 17.15"]];
+
+
+genereer_declaratie_pdf($mutatieNr, $mutatieDatum, $naam, $adres, $mailadres, $iban, $declaratieData); */
---
+++ include/pdf/3gk_declaratie_pdf.php
@@ -0,0 +1,84 @@
+<?php
+require('fpdf.php');
+
+class PDF_3GK_Declaratie extends FPDF {
+	# Page header
+	function Header() {
+    global $cfgMarge, $title;
+    $breedte = $this->GetPageWidth();
+        
+		# Stel marges in
+    $this->SetMargins($cfgMarge, $cfgMarge);
+
+    # Arial bold 15
+    $this->SetY(0);
+    $this->SetX(0); 
+    $this->SetFont('Arial','B',20);
+    $this->SetFillColor(255,255,255);
+    $this->SetDrawColor(43,21,59);
+    $this->SetLineWidth(0.5);
+    $this->Cell(75, 30, "", 'RB', 0, 'C', 1);
+    $this->SetTextColor(255,255,255);
+    $this->SetFillColor(43,21,59);
+    $this->Cell($breedte-75, 30, "DECLARATIE", 'LB', 0, 'C', 1);
+
+    # Logo
+    $size = array(70, 10);
+    $offsetX = $cfgMarge;
+    $offsetY = $cfgMarge-10;
+    $this->Image('../images/logoKoningsKerk.png',$offsetX - 20,$offsetY - 9,$size[0]);
+       
+    # Move to the right + Title
+    $this->SetY($offsetY+5);
+    $this->SetX($offsetX+$size[0]+10);    
+    
+    # Line break
+    $this->Ln($size[1]+10);
+	}
+
+	// Page footer
+	function Footer() {		
+    global $cfgMarge, $title;
+    
+    $iconXOffset = 18;
+    $iconYOffset = 11;
+				
+    $breedte = $this->GetPageWidth();
+    $hoogte = $this->GetPageHeight();
+		
+    # 2 cm van de onderkant en lettertype
+    $this->SetY(-30);
+    $this->SetX(0);
+    $this->SetFont('Helvetica','B',8);
+    $this->SetTextColor(255,255,255);
+    $this->SetFillColor(43,21,59);
+    $this->Cell($breedte, 30, "", 0, 0, 'C', 1);
+
+    # Maak contact informatie veld
+    $this->SetY(-30);
+    $this->SetX(0);
+    $this->SetFillColor(140,25,116);
+    $this->SetDrawColor(255,255,255);
+    $this->SetLineWidth(0.5);
+    $this->Cell(75, 30, "", 'R', 0, 'C', 1);
+    $this->Image('../images/e-mail.png', ($cfgMarge - $iconXOffset), ($hoogte - ($iconYOffset + 12)), 4, 4);
+    $this->Image('../images/telefoon.png', ($cfgMarge - $iconXOffset), ($hoogte - ($iconYOffset + 6)), 4, 4);
+    $this->Image('../images/adres.png', ($cfgMarge - $iconXOffset), ($hoogte - ($iconYOffset + 0)), 4, 4);
+
+    $this->SetY(-(13+$iconYOffset));
+    $this->SetX(($cfgMarge - $iconXOffset) + 5);
+   
+    $this->Cell(70, 7,'penningmeester@3gk.nl',0,0,'L');
+    $this->Cell(120,7,'KONINGSKERK DEVENTER',0,0,'R');
+    $this->Ln();
+    $this->SetX(($cfgMarge - $iconXOffset) + 5);
+    $this->Cell(70, 6,'0612345678',0,0,'L');
+    $this->Ln();
+    $this->SetX(($cfgMarge - $iconXOffset) + 5);
+    $this->Cell(70, 6,'Marienburghstraat 4, 7415 BP Deventer',0,0,'L');
+    $this->Cell(60, 6,'Pagina '.$this->PageNo().' van {nb}',0,0,'C'); 
+    $this->Cell(60, 6,strftime("%A %d %B %Y"),0,0,'R');
+	}
+}
+
+?>
