610c88b0d7c49be471b8d20012d9bd5ba3e15f20
Matthijs Draijer <git@draijer.org>
2025-11-05 22:04:53 +0100

    Documentatie LaPostaData

--- Classes/LaPostaData.php
+++ Classes/LaPostaData.php
@@ -4,22 +4,72 @@
  * Class om de lokale administratie voor LaPosta bij te houden
  */
 class LaPostaData {
+    /**
+     * @var int ID in Scipio
+     */
     public int $id;
+    /**
+     * @var bool Komt deze voor in de lokale administratie (false) of is deze nieuw (true)
+     */
     public bool $new;
+    /**
+     * @var string Status van dit lid (actief, uitgeschreven, vertrokken, etc.)
+     */
     public string $status;
+    /**
+     * @var string Geslacht (M/V)
+     */
     public string $geslacht;
+    /**
+     * @var string Voornaam
+     */
     public string $voornaam;
+    /**
+     * @var string Tussenvoegsel
+     */
     public string $tussenvoegsel;
+    /**
+     * @var string Achternaam
+     */
     public string $achternaam;
+	/**
+	 * @var string E-mailadres
+	 */
 	public string $mail;
+	/**
+	 * @var string Wijk
+	 */
 	public string $wijk;
+    /**
+     * @var string Doop- of belijdend-lid
+     */
     public string $doop;
+    /**
+     * @var string Welke relatie heeft deze persoon (gehuwd, alleenstaand ed)
+     */
     public string $relatie;
+	/**
+	 * @var string Is dit lid 70+
+	 */
 	public string $zeventigPlus;
+    /**
+     * @var int UNIX-tijd waarop dit lid voor het laatst gezien is
+     */
     public int $lastSeen;
+    /**
+     * @var int UNIX-tijd waarop dit lid voor het laatst gecontroleerd is
+     */
     public int $lastChecked;
+    /**
+     * @var int Aantal uur nadat iemand voor het laatst gezien is, hij/zij als uitgeschreven beschouwd moet worden
+     */
     private int $deadline;
 
+    /**
+     * Creer een LaPostaData-object met informatie vanuit de lokale LaPosta-database
+     * Als ID niet bestaat of niet bekend is, wordt de eigenschap 'new' op True gezet
+     * @param mixed $id
+     */
     public function __construct($id) {
         $db = new Mysql();
 
@@ -27,8 +77,9 @@ class LaPostaData {
 				
 		if($id > 0) {			
             $this->id = $id;
+            $this->new = true;
             
-			$data = $db->select("SELECT * FROM `lp_data` WHERE `scipio_id` = ". $this->id, true);
+			$data = $db->select("SELECT * FROM `lp_data` WHERE `scipio_id` = ". $this->id);
 
             if(count($data) > 0) {
                 $this->status = $data['status'];
@@ -41,14 +92,20 @@ class LaPostaData {
                 $this->relatie = $data['relatie'];
                 $this->doop = $data['doop'];
 	            $this->zeventigPlus = ($data['70_plus'] == 1 ? true : false);
+                $this->lastSeen = $data['last_seen'];
+                $this->lastChecked = $data['last_checked'];
                 $this->new = false;
-            } else {
-                $this->new = true;
             }
         }
         
     }
 
+    /**
+     * Controleer of dit mailadres recent nog door een actief lid gebruikt wordt.
+     * Met name van belang bij echtparen waarbij bv de een overlijd en de ander het mailadres overneemt.
+     * Dan moet het mailadres niet uitgeschreven worden
+     * @return bool Is het adres uniek/wordt maar door 1 iemand gebruikt?
+     */
     public function isUnique() : bool {
         $db = new Mysql();
         $deadline = mktime ((date('H')-$this->deadline));
@@ -65,6 +122,10 @@ class LaPostaData {
         
     }
 
+    /**
+     * Sla het LaPostaData-object op in de database
+     * @return bool Succesvol of niet
+     */
     public function save() {
         $db = new Mysql();
 
@@ -95,6 +156,12 @@ class LaPostaData {
     }
 
 
+   /**
+    * Geef een array terug met alle mailadressen die actief zijn, maar niet gezien zijn.
+    * @param int $aantal Maximaal aantal mailadressen (om de LaPosta-API niet te overbelasten)
+    * 
+    * @return array Array met ID's van 'oude' adressen
+    */
     static function getOldAdresses($aantal = 4) {
         $db = new Mysql();
 
