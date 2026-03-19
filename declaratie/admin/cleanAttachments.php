9f8997e8f4912beb0101085b83b1e1df7fe803b6
Matthijs Draijer <git@draijer.org>
2026-03-19 19:22:52 +0100

    Cron-ready

--- declaratie/admin/cleanAttachments.php
+++ declaratie/admin/cleanAttachments.php
@@ -7,86 +7,84 @@ include_once('../../Classes/Declaratie.php');
 include_once('../../Classes/Member.php');
 include_once('../../Classes/Logging.php');
 
-$requiredUserGroups = array(1);
-$cfgProgDir = '../../auth/';
-include($cfgProgDir. "secure.php");
-
-# Status waarvan de bijlages sowieso bewaard moeten blijven
-# @see Declaratie::status voor de omschrijving van elke status
-$keepStatus = array(0, 1, 2, 3, 4);
-
-# Status waarvan de bijlages na verloop van tijd verwijderd mogen worden
-$cleanStatus = array(5, 6, 7, 8);
-
-# Bijlages die ouder zijn dan 3 maanden (en status uit $cleanStatus hebben) mogen verwijderd wordne
-$grens = mktime(0,0,0,date('n')-3);
-
-# Directories
-$uploadDir      = '../uploads/';
-$uploadDirNew   = '../uploads_old/'. date('Y').'.'.substr('0'.date('n'), -2) .'/';
-
-# Nieuwe directory aanmaken
-if(!file_exists($uploadDirNew)) {
-    mkdir($uploadDirNew);
-
-    $index = fopen($uploadDirNew.'index.php', 'w');
-    fwrite($index, '<?php'."\n");
-    fwrite($index, '$url="Location: ../../";'."\n");
-    fwrite($index, 'header($url);'."\n");
-    fclose($index);
-}
-
 # Initialiseren
 $rescue = $page = array();
+$test = false;
 
-# Sowieso bewaren
-foreach($keepStatus as $status) {
-    $hashes = Declaratie::getDeclaraties($status);
+if(in_array($_SERVER['REMOTE_ADDR'], $allowedIP) OR $test) {
+    # Status waarvan de bijlages sowieso bewaard moeten blijven
+    # @see Declaratie::status voor de omschrijving van elke status
+    $keepStatus = array(0, 1, 2, 3, 4);
 
-    foreach($hashes as $hash) {
-        $declaratie = new Declaratie($hash);
-        
-        foreach($declaratie->bijlagen as $file => $name) {
-            $rescue[] = $file;
-        }
+    # Status waarvan de bijlages na verloop van tijd verwijderd mogen worden
+    $cleanStatus = array(5, 6, 7, 8);
+
+    # Bijlages die ouder zijn dan 3 maanden (en status uit $cleanStatus hebben) mogen verwijderd wordne
+    $grens = mktime(0,0,0,date('n')-3);
+
+    # Directories
+    $uploadDir      = '../uploads/';
+    $uploadDirNew   = '../uploads_old/'. date('Y').'.'.substr('0'.date('n'), -2) .'/';
+
+    # Nieuwe directory aanmaken
+    if(!file_exists($uploadDirNew)) {
+        mkdir($uploadDirNew);
+
+        $index = fopen($uploadDirNew.'index.php', 'w');
+        fwrite($index, '<?php'."\n");
+        fwrite($index, '$url="Location: ../../";'."\n");
+        fwrite($index, 'header($url);'."\n");
+        fclose($index);
     }
-}
 
-# Bewaren als niet te oud
-foreach($cleanStatus as $status) {
-    $hashes = Declaratie::getDeclaraties($status);
+    # Sowieso bewaren
+    foreach($keepStatus as $status) {
+        $hashes = Declaratie::getDeclaraties($status);
 
-    foreach($hashes as $hash) {
-        $declaratie = new Declaratie($hash);
-
-        # Als de laatste actie minder dan 3 maanden geleden is
-        # Bewaar de bijlage
-        if($declaratie->lastAction > $grens) {            
+        foreach($hashes as $hash) {
+            $declaratie = new Declaratie($hash);
+            
             foreach($declaratie->bijlagen as $file => $name) {
                 $rescue[] = $file;
             }
         }
     }
-}
 
-# Maak een array met alle bestanden in de upload-map
-$upload = opendir($uploadDir);
-while (false !== ($entry = readdir($upload))) {
-    if ($entry === '.' || $entry === '..') {
-        continue;
+    # Bewaren als niet te oud
+    foreach($cleanStatus as $status) {
+        $hashes = Declaratie::getDeclaraties($status);
+
+        foreach($hashes as $hash) {
+            $declaratie = new Declaratie($hash);
+
+            # Als de laatste actie minder dan 3 maanden geleden is
+            # Bewaar de bijlage
+            if($declaratie->lastAction > $grens) {            
+                foreach($declaratie->bijlagen as $file => $name) {
+                    $rescue[] = $file;
+                }
+            }
+        }
     }
-    $files[] = $entry;
-}
 
-# Doorloop alle bestanden in de upload-directory
-foreach($files as $file) {
-    # En kijk of ze voorkomen in de lijst met te bewaren bijlages
-    # Zo ja, bewaar ze
-    # Zo nee, verwijder ze
-    if(!in_array('uploads/'.$file, $rescue)) {        
-        $page[] = $file .' verplaatst<br>';
-        rename($uploadDir.$file, $uploadDirNew.$file);
-        toLog('declaratie-bijlage '. $file .' verwijderd', 'debug');
+    # Maak een array met alle bestanden in de upload-map
+    $upload = opendir($uploadDir);
+    while (false !== ($entry = readdir($upload))) {
+        if ($entry !=  '.' && $entry != '..' && $entry != 'index.php') {
+            $files[] = $entry;
+        }
+    }
+
+    # Doorloop alle bestanden in de upload-directory
+    foreach($files as $file) {
+        # En kijk of ze voorkomen in de lijst met te bewaren bijlages
+        # Zo ja, bewaar ze
+        # Zo nee, verwijder ze
+        if(!in_array('uploads/'.$file, $rescue)) {        
+            $page[] = $file .' verplaatst<br>';
+            rename($uploadDir.$file, $uploadDirNew.$file);
+            toLog('declaratie-bijlage '. $file .' verwijderd', 'debug');
+        }
     }
 }
 
